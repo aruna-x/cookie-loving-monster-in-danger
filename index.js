940762fa2354787b710826fa9851bfcf54c1a4ca
Aruna <aruna.evan15@gmail.com>
2021-10-07 16:19:14 -0700

    Added confetti upon win, added win pic, added animation for forward movement

--- index.js
+++ index.js
@@ -108,10 +108,13 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(`https://jservice.io/api/category?id=${category_id}`)
             .then(r => r.json())
             .then(categoryObj => {
-                let clueList = categoryObj.clues.filter(x => {
-                    return ((x.value <= difficultyValue) && (x.value > difficultyValue - 200));
+                let clueList = categoryObj.clues.filter(clue => {
+                    const difficultyBool = ((clue.value <= difficultyValue) && (clue.value > difficultyValue - 200));
+                    const vettedQuestion = !(clue.question.toLowerCase().includes("as seen in"));
+                    return (difficultyBool && vettedQuestion);
                 });
-                question = clueList[Math.floor(Math.random() * clueList.length)];
+
+                const question = clueList[Math.floor(Math.random() * clueList.length)];
                 placeQuestion(question);
             })
             .catch(e => console.error(`There was an error with fetch in apiCall: ${e}`));
@@ -155,11 +158,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 if (checkMatch) {
                     moveMonster(monsterMove, "forward");
-                    postQuestionButton("green");
+                    postQuestionButton("right");
                 }
                 else {
                     moveMonster(monsterMove, "backward");
-                    postQuestionButton("red");
+                    postQuestionButton("wrong");
                 }
             }
             else {
@@ -171,12 +174,11 @@ document.addEventListener("DOMContentLoaded", () => {
             function postQuestionButton(buttonColor) {
                 document.getElementById('answer').style.display = "none";
                 document.getElementById('submit').textContent = "Try another one!";
-                document.getElementById('submit').className = "back-to-board";
-                if (buttonColor==="red") {
-                    document.getElementById('submit').style.background="rgba(255, 0, 0, 0.3)";
+                if (buttonColor==="wrong") {
+                    document.getElementById('submit').className = "answer-wrong";
                 } 
-                else if (buttonColor === "green") {
-                    document.getElementById('submit').style.background="rgba(8, 141, 8, 0.514)";
+                else if (buttonColor === "right") {
+                    document.getElementById('submit').className = "answer-right";
                 }
             }
         });
@@ -185,14 +187,20 @@ document.addEventListener("DOMContentLoaded", () => {
             let monsterLeft = document.getElementById("cookie-monster").style.marginLeft;
             let monsterLeftNum = parseInt(monsterLeft, 10);
             if (direction === "forward"){
-                document.getElementById('question').textContent = "That much closer to the cookies!";
+                document.getElementById('question').textContent = "Phew! Correct. That much closer to the cookies!";
                 const left = monsterLeftNum + monsterMove;
                 if (left>=90){
                     document.getElementById("cookie-monster").style.marginLeft = `90%`;
-                    // TODO Trigger the celebration!!! Party time.
+                    confetti();
+                    document.getElementById('question').textContent = "You did it! And I think you made a friend along the way!!";
+                    document.getElementById('monster-win-wrap').style.display = "contents";
+                    document.getElementById('wrap-question').style.height = "375px";
+                    document.getElementById('submit').style.visibility = "hidden";
                 }
                 else {
                     document.getElementById("cookie-monster").style.marginLeft = `${left}%`;
+                    document.getElementById("cookie-monster").classList.add('wiggle');
+                    setTimeout(()=>{document.getElementById("cookie-monster").classList.remove('wiggle');}, 3000);
                 }
             }
             else if (direction === "backward"){
@@ -203,6 +211,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 else {
                     document.getElementById("cookie-monster").style.marginLeft = `${left}%`;
+                    document.getElementById("cookie-monster").classList.add('up-and-down');
+                    setTimeout(()=>{document.getElementById("cookie-monster").classList.remove('up-and-down');}, 3000);
                 }
             }
         }
@@ -321,4 +331,195 @@ function commonSubstring(s1, s2){
             }
         }
     }
+}
+
+function confetti() {
+    class Progress {
+    constructor(param = {}) {
+      this.timestamp        = null;
+      this.duration         = param.duration || Progress.CONST.DURATION;
+      this.progress         = 0;
+      this.delta            = 0;
+      this.progress         = 0;
+      this.isLoop           = !!param.isLoop;
+
+      this.reset();
+    }
+
+    static get CONST() {
+      return {
+        DURATION : 1000
+      };
+    }
+
+    reset() {
+      this.timestamp = null;
+    }
+
+    start(now) {
+      this.timestamp = now;
+    }
+
+    tick(now) {
+      if (this.timestamp) {
+        this.delta    = now - this.timestamp;
+        this.progress = Math.min(this.delta / this.duration, 1);
+
+        if (this.progress >= 1 && this.isLoop) {
+          this.start(now);
+        }
+
+        return this.progress;
+      } else {
+        return 0;
+      }
+    }
+    }
+
+    class Confetti {
+    constructor(param) {
+      this.parent         = param.elm || document.body;
+      this.canvas         = document.createElement("canvas");
+      this.ctx            = this.canvas.getContext("2d");
+      this.width          = param.width  || this.parent.offsetWidth;
+      this.height         = param.height || this.parent.offsetHeight;
+      this.length         = param.length || Confetti.CONST.PAPER_LENGTH;
+      this.yRange         = param.yRange || this.height * 2;
+      this.progress       = new Progress({
+        duration : param.duration,
+        isLoop   : true
+      });
+      this.rotationRange  = typeof param.rotationLength === "number" ? param.rotationRange
+                                                                      : 10;
+      this.speedRange     = typeof param.speedRange     === "number" ? param.speedRange
+                                                                      : 10;
+      this.sprites        = [];
+
+      this.canvas.style.cssText = [
+        "display: block",
+        "position: absolute",
+        "top: 0",
+        "left: 0",
+        "pointer-events: none"
+      ].join(";");
+
+      this.render = this.render.bind(this);
+
+      this.build();
+
+      this.parent.appendChild(this.canvas);
+      this.progress.start(performance.now());
+
+      requestAnimationFrame(this.render);
+    }
+
+    static get CONST() {
+      return {
+          SPRITE_WIDTH  : 13,
+          SPRITE_HEIGHT : 20,
+          PAPER_LENGTH  : 100,
+          DURATION      : 20000,
+          ROTATION_RATE : 50,
+          COLORS        : [
+            "#EF5350",
+            "#EC407A",
+            "#AB47BC",
+            "#7E57C2",
+            "#5C6BC0",
+            "#42A5F5",
+            "#29B6F6",
+            "#26C6DA",
+            "#26A69A",
+            "#66BB6A",
+            "#9CCC65",
+            "#D4E157",
+            "#FFEE58",
+            "#FFCA28",
+            "#FFA726",
+            "#FF7043",
+            "#8D6E63",
+            "#BDBDBD",
+            "#78909C"
+          ]
+      };
+    }
+
+    build() {
+      for (let i = 0; i < this.length; ++i) {
+        let canvas = document.createElement("canvas"),
+            ctx    = canvas.getContext("2d");
+
+        canvas.width  = Confetti.CONST.SPRITE_WIDTH;
+        canvas.height = Confetti.CONST.SPRITE_HEIGHT;
+
+        canvas.position = {
+          initX : Math.random() * this.width,
+          initY : -canvas.height - Math.random() * this.yRange
+        };
+
+        canvas.rotation = (this.rotationRange / 2) - Math.random() * this.rotationRange;
+        canvas.speed    = (this.speedRange / 2) + Math.random() * (this.speedRange / 2);
+
+        ctx.save();
+          ctx.fillStyle = Confetti.CONST.COLORS[(Math.random() * Confetti.CONST.COLORS.length) | 0];
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.restore();
+
+        this.sprites.push(canvas);
+      }
+    }
+
+    render(now) {
+      let progress = this.progress.tick(now);
+
+      this.canvas.width  = this.width;
+      this.canvas.height = this.height;
+
+      for (let i = 0; i < this.length; ++i) {
+        this.ctx.save();
+          this.ctx.translate(
+            this.sprites[i].position.initX + this.sprites[i].rotation * Confetti.CONST.ROTATION_RATE * progress,
+            this.sprites[i].position.initY + progress * (this.height + this.yRange)
+          );
+          this.ctx.rotate(this.sprites[i].rotation);
+          this.ctx.drawImage(
+            this.sprites[i],
+            -Confetti.CONST.SPRITE_WIDTH * Math.abs(Math.sin(progress * Math.PI * 2 * this.sprites[i].speed)) / 2,
+            -Confetti.CONST.SPRITE_HEIGHT / 2,
+            Confetti.CONST.SPRITE_WIDTH * Math.abs(Math.sin(progress * Math.PI * 2 * this.sprites[i].speed)),
+            Confetti.CONST.SPRITE_HEIGHT
+          );
+        this.ctx.restore();
+      }
+
+      requestAnimationFrame(this.render);
+    }
+    }
+
+    let body = document.body;
+    let html = document.documentElement;
+
+    let height = Math.max( body.scrollHeight, body.offsetHeight, 
+                      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    (() => {
+    const DURATION = 20000,
+          LENGTH   = 120;
+
+    new Confetti({
+      width    : window.innerWidth,
+      height   : height,
+      length   : LENGTH,
+      duration : DURATION
+    });
+
+    setTimeout(() => {
+      new Confetti({
+        width    : window.innerWidth,
+        height   : height,
+        length   : LENGTH,
+        duration : DURATION
+      });
+    }, DURATION / 2);
+    })();
 }
